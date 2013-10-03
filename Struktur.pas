03c26284858d90ea216b0f8159b79632a9717edc
ArneKa <arne_kaufmann@hotmail.de>
2013-10-03 20:13:33 +0200

    Menü
    Hey, wie findet ihr die von mir vorgeschlagene Menüaufteilung?

--- Struktur.pas
+++ Struktur.pas
@@ -4,10 +4,15 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls;
+  StdCtrls, ExtCtrls, Math, jpeg;
 
 type
   TMenue = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
     procedure FormPaint(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X,
@@ -16,6 +21,10 @@ type
       Shift: TShiftState; X, Y: Integer);
     procedure FormMouseUp(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
+    function Kreisposition_x(Objektnummer:integer;Objektanzahl:integer;
+             Zentrum:TPoint;Radius:integer): integer;
+    function Kreisposition_y(Objektnummer:integer;Objektanzahl:integer;
+             Zentrum:TPoint;Radius:integer): integer;
   private
     { Private-Deklarationen }
   public
@@ -32,8 +41,80 @@ implementation
 {$R *.DFM}
 
 procedure TMenue.FormPaint(Sender: TObject);
+var i : integer;
+    x,y : integer;
+    ScreenMitte: TPoint;
+    Anzahl:integer;
+    Radius_x,Radius_y : integer;
+    Buttonbreite : integer;
 begin
      Menue.Color := Themenfarbe1;
+
+     ButtonBreite := Screen.Width div 6;                    // Buttonbreite wird in Abhängigkeit der
+     Image1.Width := ButtonBreite;                          // Bildschrimgröße bestimmt
+     Image1.Height := ButtonBreite;
+     Image2.Width := ButtonBreite;                          // provisorisch habe ich Images als Platzhalter
+     Image2.Height := ButtonBreite;                         // für die späteren Butten genommen...
+     Image3.Width := ButtonBreite;                          // sie könnten Bilder enthälten, Zeichen, nur Text
+     Image3.Height := ButtonBreite;                         // Form steht auch nicht fest...
+     Image4.Width := ButtonBreite;                          // Das Alles wird sich alles im Laufe der
+     Image4.Height := ButtonBreite;                         // Programmentwicklung noch ergeben.
+     Image5.Width := ButtonBreite;
+     Image5.Height := ButtonBreite;
+                                                                      // im Folgenden werden die Menüpunkte im Kreis(Ellipse) angeordnet:
+     ScreenMitte := Point(Screen.Width div 2,Screen.Height div 2);    // Mitte des Screen wird ermittelt
+     Anzahl := 5;                                                     // Anzahl der Menüobjekte
+     Radius_x := Screen.Width div 3;                                  // Radius in x-Richtung
+     Radius_y := Screen.Height div 3;                                 // Radius in y-Richtung
+     for i := 1 to Anzahl do
+     begin
+          x := Kreisposition_x(i,Anzahl,ScreenMitte,Radius_x);        // x- und y-Koordinate für das i-te Objekt wird ermittelt
+          y := Kreisposition_y(i,Anzahl,ScreenMitte,Radius_y);        // dabei werden oben bestimmte Parameter übergeben
+          case i of
+          1: begin
+                  Image1.Left := x - ButtonBreite div 2;              // den Menüobjekten werden ihre Koordinaten zugeordnet
+                  Image1.Top  := y - ButtonBreite div 2;
+             end;
+          2: begin
+                  Image2.Left := x - ButtonBreite div 2;
+                  Image2.Top  := y - ButtonBreite div 2;
+             end;
+          3: begin
+                  Image3.Left := x - ButtonBreite div 2;
+                  Image3.Top  := y - ButtonBreite div 2;
+             end;
+          4: begin
+                  Image4.Left := x - ButtonBreite div 2;
+                  Image4.Top  := y - ButtonBreite div 2;
+             end;
+          5: begin
+                  Image5.Left := x - ButtonBreite div 2;
+                  Image5.Top  := y - ButtonBreite div 2;
+             end;
+          end;
+     end;
+end;
+
+function TMenue.Kreisposition_x(Objektnummer:integer;Objektanzahl:integer;Zentrum:TPoint;
+         Radius:integer) : integer;                            // Parameterübergabe
+var
+   RadWinkel  : real;
+   x          : integer;
+begin
+     RadWinkel := Objektnummer*((2*pi)/Objektanzahl);          // Winkel des aktuellen Objektes wird errechnet
+     x := round(Zentrum.x + cos(RadWinkel+(pi/2))*Radius);     // daraus wird die x-Koordinate des Objektes ermittelt
+     result := x;
+end;
+
+function TMenue.Kreisposition_y(Objektnummer:integer;Objektanzahl:integer;Zentrum:TPoint;
+         Radius:integer) : integer;                            // Parameterübergabe
+var
+   RadWinkel  : real;
+   y          : integer;
+begin
+     RadWinkel := Objektnummer*((2*pi)/Objektanzahl);          // Winkel des aktuellen Objektes wird errechnet
+     y := round(Zentrum.y - sin(RadWinkel+(pi/2))*Radius);     // daraus wird die y-Koordinate des Objektes ermittelt
+     result := y;
 end;
 
 procedure TMenue.FormCreate(Sender: TObject);
@@ -48,12 +129,12 @@ procedure TMenue.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
   Y: Integer);
 var dif:integer;
 begin
-     if Y < 50 then                              // Wenn sich die Maus im oberen Bildschirmbereich
+     if Y < 10 then                              // Wenn sich die Maus im oberen Bildschirmbereich
      begin
           Cursor := crHandpoint;                 // befindet, bekommt sie ein Handsymbol und
           Canvas.Brush.Color := Themenfarbe2;
           Canvas.Pen.Color   := Themenfarbe2;    // mit Canvas wird der obere Bildschirmbereich
-          Canvas.Rectangle(0,0,ClientWidth,50);  // in der 2. Themenfarbe gefärbt.
+          Canvas.Rectangle(0,0,ClientWidth,10);  // in der 2. Themenfarbe gefärbt.
      end else
      begin                                       // andererseits wird,
           if Cursor = crHandpoint then           // nur wenn es nicht schon der Fall ist,
@@ -68,6 +149,8 @@ begin
           dif := Mouse.CursorPos.y -Y;        
           Menue.Top := Y+dif;                 // verschiebt sich das Fenster mit der Maus in y-Richtung.
      end;
+
+     if Menue.Top > (Screen.Height div 6) * 5 then close;  // Sicherheitsschließen
 end;
 
 
@@ -78,8 +161,8 @@ begin
      begin
           if Cursor = crHandpoint then       // und sich die Maus im oberen Bildschirmberech befindet (s.o.),
           begin                              // wird aus dem Vollbild "Normalbild" (ist aber nicht sichtbar,
-          end;                               // da dich Höhe und Breite des Fenstern NICHT ändert!
-               Menue.Align := alNone;        // Dies muss aber geschehen, damit man das Fenster im
+               Menue.Align := alNone;        // da dich Höhe und Breite des Fenstern NICHT ändert!
+          end;                               // Dies muss aber geschehen, damit man das Fenster im
      end;                                    // nächsten Schritt verschieben kann.
 end;
 
@@ -87,26 +170,27 @@ procedure TMenue.FormMouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 var i: byte;
 begin
-     if Button = mbleft then                             // linke Taste wird gelöst:
+     if (Button = mbleft) and (Cursor = crHandpoint) then// linke Taste wird gelöst:
      begin
           if Menue.Top > Screen.Height div 2 then        // Wenn das Fenster bis unter die Hälfte 
           begin                                          // der Bildschirmhöhe gezogen wurde,
                for i := Y to Screen.Height - 100 do      // wird das Menü bis zum Verschwinden
-               begin                                     
+               begin
                     Menue.Top := Menue.Top + i;          // weiter nach unten verschoben
                     sleep(10);                           // (nicht sofort, sondern "langsam")
                end;
                close;                                    // und schließlich geschlossen.
           end else                                       // Wird das Fenster nicht genügend weit
           begin                                          // herunter gezogen,
-               for i := Mouse.CursorPos.y downto 0 do
+               {for i := Mouse.CursorPos.y downto 0 do
                begin
                     Menue.Top := i;                      // gelangt das Fenster wieder "langsam" in
-                    sleep(1);                            // die Ausgangsposition und zum Schluss wieder
-               end;
+                    //sleep(1);                            // die Ausgangsposition und zum Schluss wieder
+               end;  }
                Menue.Align := alClient;                  // in den unverschiebbaren Vollbildmodus
           end;
      end;
 end;
+
 
 end.
